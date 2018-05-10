9201d32b135970147a10c56e60cc32e133791df3
dermen <dermen@stanford.edu>
2018-05-10 14:45:31 -0500

    asdlk

--- range_slider.py
+++ range_slider.py
@@ -49,36 +49,24 @@ class RangeSlider(tk.Frame):
         self.range_slider_token_RHS_offset = 30
         self.text_offset_LHS = 5
         self.text_offset_RHS = -5
-        #self.range_slider_value_range = self.range_slider_len \
-        #    -2*self.range_slider_token_halfwidth - \
-        #    self.range_slider_token_LHS_offset\
-        #    -self.range_slider_token_RHS_offset
+        self.range_slider_value_range = self.range_slider_len \
+            -2*self.range_slider_token_width - self.range_slider_token_LHS_offset\
+            -self.range_slider_token_RHS_offset
         
-        self.range_slider_maxRHS = self.range_slider_len - \
-            self.range_slider_token_halfwidth - \
-            self.range_slider_token_RHS_offset
+        self.range_slider_maxRHS = self.range_slider_len - self.range_slider_token_halfwidth - self.range_slider_token_RHS_offset
 
-        self.range_slider_minLHS = self.range_slider_token_halfwidth+\
-             self.range_slider_token_LHS_offset
+        self.range_slider_minLHS = self.range_slider_token_halfwidth + self.range_slider_token_LHS_offset
         
-        self.range_slider_value_range = self.range_slider_maxRHS- \
-            self.range_slider_minLHS
-
 #       create a canvas for drawing the range-slider...
         self.canvas_frame = tk.Frame( self.master) 
-        self.canvas = tk.Canvas(self.canvas_frame, 
-            width=self.range_slider_len, 
-            height=self.range_slider_height, 
-            bg='white')
+        self.canvas = tk.Canvas(self.canvas_frame, width=self.range_slider_len, 
+            height=self.range_slider_height, bg='white')
        
 #       reate horizontal line sepcifying the slider token track
-        self.canvas.create_line( ( \
-            self.range_slider_token_LHS_offset, 
-            self.range_slider_token_vertical_offset+\
-                self.range_slider_token_halfheight, 
+        self.canvas.create_line( ( self.range_slider_token_LHS_offset, 
+            self.range_slider_token_vertical_offset+self.range_slider_token_halfheight, 
             self.range_slider_len-self.range_slider_token_RHS_offset, 
-            self.range_slider_token_vertical_offset+\
-                self.range_slider_token_halfheight) )# fill='white')
+            self.range_slider_token_vertical_offset+self.range_slider_token_halfheight) )# fill='white')
         
 #       this data is used to keep track the slider components as they are dragged
         self._drag_data = {"x": 0, "y": 0, "item": None}
@@ -86,12 +74,10 @@ class RangeSlider(tk.Frame):
 #       create a couple of movable sliders
 #       SLIDER TOKENS 
         i1 = self._create_token((self.range_slider_minLHS, 
-            self.range_slider_token_vertical_offset+\
-                self.range_slider_token_halfheight), 
+            self.range_slider_token_vertical_offset+self.range_slider_token_halfheight), 
             self.color)
         i2 = self._create_token((self.range_slider_maxRHS,
-            self.range_slider_token_vertical_offset+\
-                self.range_slider_token_halfheight), 
+            self.range_slider_token_vertical_offset+self.range_slider_token_halfheight), 
             self.color)
         self.items = [i1, i2] # store the IDs
 
@@ -99,33 +85,23 @@ class RangeSlider(tk.Frame):
 
         # add bindings for clicking, dragging and releasing over
         # any object with the "token" tag
-        self.canvas.tag_bind("token", 
-            "<ButtonPress-1>", 
-            self.on_token_press)
-        self.canvas.tag_bind("token", 
-            "<ButtonRelease-1>", 
-            self.on_token_release)
-        self.canvas.tag_bind("token", 
-            "<B1-Motion>", 
-            self.on_token_motion)
+        self.canvas.tag_bind("token", "<ButtonPress-1>", self.on_token_press)
+        self.canvas.tag_bind("token", "<ButtonRelease-1>", self.on_token_release)
+        self.canvas.tag_bind("token", "<B1-Motion>", self.on_token_motion)
         
         self._pack_widgets()
 
     def _pack_widgets(self):
         self.entry_minval.pack(side=tk.LEFT)
-        self.canvas.pack(expand=True, side=tk.LEFT, padx=0)
+        self.canvas.pack(expand=True, side=tk.LEFT, padx=10)
         self.canvas_frame.pack(side=tk.LEFT,)
         self.entry_maxval.pack(side=tk.LEFT)
 
     def _create_min_max_entry(self):
-        self.entry_minval = tk.Entry(self.master, 
-            bd=2, 
+        self.entry_minval = tk.Entry(self.master, bd=2, 
             relief=tk.GROOVE, 
             width=10)
-        self.entry_maxval = tk.Entry(self.master, 
-            bd=2, 
-            relief=tk.GROOVE, 
-            width=10)
+        self.entry_maxval = tk.Entry(self.master, bd=2, relief=tk.GROOVE, width=10)
         self.entry_minval.bind('<Return>', self._get_min_max_vals )
         self.entry_maxval.bind('<Return>', self._get_min_max_vals )
 
@@ -134,85 +110,22 @@ class RangeSlider(tk.Frame):
         dmax = float( self.entry_maxval.get() )
         
         assert( dmin < dmax)
-        # left item
+        
+        # move the left item
         if self.dmin != dmin:
-            self.minval = dmin
-            self.dmin = dmin
-        
-        #  right item 
-        if self.dmax != dmax:
-            self.dmax = dmax
-            self.maxval = dmax
-        
-        left_x = self.range_slider_value_range * \
-            (self.minval-dmin)/(float(dmax)-dmin) \
-                + self.range_slider_minLHS
-       
-        right_x = self.range_slider_value_range *\
-            (self.maxval-dmin)/(float(dmax)-dmin) + \
-            self.range_slider_minLHS  
-        
-        coors_right = self.canvas.coords(self.items[1])
-        
-        left_item = self.items[0]
-        right_item = self.items[1]
-
-            
-        left_y = self.range_slider_token_vertical_offset+\
-                self.range_slider_token_halfheight
-            
-        x1,y1,x2,y2 = (left_x-self.range_slider_token_halfwidth, 
-                    left_y-self.range_slider_token_halfheight, 
-                    left_x+self.range_slider_token_halfwidth, 
-                    left_y+self.range_slider_token_halfheight)
-        self.canvas.coords( left_item, (x1,y1,x2,y2))
-        #   move the label
-        self.canvas.itemconfig( self.minvaltext, 
-            text="%.2f"%self.minval)
-        
-        self.canvas.coords( self.minvaltext, 
-                    (left_x-self.text_offset_LHS, 
-                    self.token_text_vertical_position))
-        
-        
-        right_y = self.range_slider_token_vertical_offset+\
-                self.range_slider_token_halfheight
-        
-        x1,y1,x2,y2 = (right_x-self.range_slider_token_halfwidth, 
-                    right_y-self.range_slider_token_halfheight, 
-                    right_x+self.range_slider_token_halfwidth, 
-                    right_y+self.range_slider_token_halfheight)
-        self.canvas.coords( right_item, (x1,y1,x2,y2))
-   
-        # move the label
-        self.canvas.itemconfig( self.maxvaltext, 
-            text="%.2f"%self.maxval)
-        
-        self.canvas.coords( self.maxvaltext, 
-            (right_x+self.text_offset_RHS, 
-                self.token_text_vertical_position))
-        
-    ###############
-    def _move_old(self):
-        if self.dmin != dmin:
-            
             self.minval = dmin
             self.dmin = dmin
             
             left_item = self.items[0]
-            
             (left_x,left_y) = (self.range_slider_minLHS, 
-                self.range_slider_token_vertical_offset+\
-                    self.range_slider_token_halfheight)
-            
+                self.range_slider_token_vertical_offset+self.range_slider_token_halfheight)
             x1,y1,x2,y2 = (left_x-self.range_slider_token_halfwidth, 
                         left_y-self.range_slider_token_halfheight, 
                         left_x+self.range_slider_token_halfwidth, 
                         left_y+self.range_slider_token_halfheight)
             self.canvas.coords( left_item, (x1,y1,x2,y2))
             #   move the label
-            self.canvas.itemconfig( self.minvaltext, 
-                text="%.2f"%self.minval)
+            self.canvas.itemconfig( self.minvaltext, text="%.2f"%self.minval)
             self.canvas.coords( self.minvaltext, 
                         (self.range_slider_minLHS-self.text_offset_LHS, 
                         self.token_text_vertical_position))
@@ -223,8 +136,7 @@ class RangeSlider(tk.Frame):
             self.maxval = dmax
             right_item = self.items[1]
             (right_x, right_y) = (self.range_slider_maxRHS,
-                self.range_slider_token_vertical_offset+\
-                    self.range_slider_token_halfheight)
+                self.range_slider_token_vertical_offset+self.range_slider_token_halfheight)
             x1,y1,x2,y2 = (right_x-self.range_slider_token_halfwidth, 
                         right_y-self.range_slider_token_halfheight, 
                         right_x+self.range_slider_token_halfwidth, 
@@ -233,14 +145,11 @@ class RangeSlider(tk.Frame):
         
        
             # move the label
-            self.canvas.itemconfig( self.maxvaltext, 
-                text="%.2f"%self.maxval)
+            self.canvas.itemconfig( self.maxvaltext, text="%.2f"%self.maxval)
             
-            self.canvas.coords( self.maxvaltext, 
-                (self.range_slider_maxRHS+self.text_offset_RHS, 
-                self.token_text_vertical_position))
-            
-    ###############
+            self.canvas.coords( self.maxvaltext, (self.range_slider_maxRHS+self.text_offset_RHS, 
+                    self.token_text_vertical_position))
+
     def _set_min_max_vals(self): 
         self.entry_minval.delete(0,tk.END)
         self.entry_minval.insert(0,"%.1f"%self.minval)
@@ -259,8 +168,7 @@ class RangeSlider(tk.Frame):
     def _create_token(self, coord, color):
         '''Create a token at the given coordinate in the given color'''
         (x,y) = coord
-        return self.canvas.create_rectangle(x-\
-                self.range_slider_token_halfwidth, 
+        return self.canvas.create_rectangle(x-self.range_slider_token_halfwidth, 
             y-self.range_slider_token_halfheight, 
             x+self.range_slider_token_halfwidth, 
             y+self.range_slider_token_halfheight, 
@@ -297,26 +205,20 @@ class RangeSlider(tk.Frame):
             x_new = min( clicked_x + delta_x, self.range_slider_maxRHS) 
             x_new = max( x_new, other_x+self.range_slider_token_width) 
 
-            self.canvas.coords( clicked_item, 
-                    (x_new-self.range_slider_token_halfwidth, 
-                    clicked_y-self.range_slider_token_halfheight, 
+            self.canvas.coords( clicked_item, (x_new-self.range_slider_token_halfwidth, 
+                clicked_y-self.range_slider_token_halfheight, 
                     x_new+self.range_slider_token_halfwidth, 
                     clicked_y+self.range_slider_token_halfheight))
-            
-            self.canvas.coords( self.maxvaltext, 
-                (x_new+self.text_offset_LHS,
+            self.canvas.coords( self.maxvaltext, (x_new+self.text_offset_LHS,
                 self.token_text_vertical_position))
+            self.canvas.itemconfig( self.maxvaltext, text="%.2f"%self.maxval)
+            
             new_drag = min( self.range_slider_maxRHS,  event.x)
             new_drag = max(  other_x , new_drag)
             self._drag_data["x"] = new_drag 
             
-            val_range = float(self.dmax-self.dmin)
-            pix_range = float(self.range_slider_value_range)
-            
-            self.maxval = (x_new / pix_range )*val_range + self.dmin
-            
-            self.canvas.itemconfig( self.maxvaltext, 
-                text="%.2f"%self.maxval)
+            self.maxval = ( (x_new) / self.range_slider_value_range ) \
+                * ( self.dmax - self.dmin ) + self.dmin
             
         else:
             #   LEFT HAND SIDE ITEM
@@ -326,28 +228,19 @@ class RangeSlider(tk.Frame):
             x_new = min( x_new, other_x-self.range_slider_token_width) 
             
             self.canvas.coords( clicked_item, 
-                    (x_new-self.range_slider_token_halfwidth, 
+                (x_new-self.range_slider_token_halfwidth, 
                     clicked_y-self.range_slider_token_halfheight, 
                     x_new+self.range_slider_token_halfwidth, 
                     clicked_y+self.range_slider_token_halfheight))
-            
-            self.canvas.coords( self.minvaltext, 
-                    (x_new-self.text_offset_RHS, 
-                    self.token_text_vertical_position))
-
+            self.canvas.coords( self.minvaltext, (x_new-self.text_offset_RHS, 
+                self.token_text_vertical_position))
+            self.canvas.itemconfig( self.minvaltext, text="%.2f"%self.minval)
             new_drag = max( self.range_slider_minLHS,  event.x)
             new_drag = min(  other_x , new_drag)
             self._drag_data["x"] = new_drag 
             
-
-            val_range = float(self.dmax-self.dmin)
-            pix_range = float(self.range_slider_value_range)
-
-            self.canvas.itemconfig( self.minvaltext, 
-                text="%.2f"%self.minval)
-            
-            self.minval = (x_new / pix_range )*val_range + self.dmin
-            
+            self.minval = ((x_new) / self.range_slider_value_range ) \
+                * ( self.dmax - self.dmin ) + self.dmin
 
 def main():
     root = tk.Tk()
